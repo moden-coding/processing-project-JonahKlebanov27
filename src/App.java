500d89d9f83c24a6b75c8b92bdabffe506b1192d
Jklebanovgithub <159466591+Jklebanovgithub@users.noreply.github.com>
2024-10-01 09:20:43 -0400

    Final project draft

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ public class App extends PApplet {
     float x1, y1, x2, y2, x3, y3;
     float speed = 20;
 
-    float lineX1 = 600, lineY1 = 100, lineX2 = 100, lineY2 = 100;
+    float lineX1 = 100, lineY1 = 600, lineX2 = 100, lineY2 = 100;
 
     public static void main(String[] args) {
         PApplet.main("App");
@@ -32,29 +32,30 @@ public class App extends PApplet {
         fill(0, 90, 175);
         stroke(200, 0, 0);
         strokeWeight(5);
-        triangle(x1, y1, x2, y2, x3, y3); 
+        triangle(x1, y1, x2, y2, x3, y3);
 
         stroke(0);
         strokeWeight(2);
-        line(100,600,100,100);
-       
-        
+        line(lineX1,lineY1,lineX2,lineY2);
 
     }
 
     public void keyPressed() {
-        
+        System.out.println("key pressed");
 
         if (keyCode == UP && !willTriangleCrossLine(0, -speed)) {
-            moveTriangle(0, -speed);  
+
+            moveTriangle(0, -speed);
         } else if (keyCode == DOWN && !willTriangleCrossLine(0, speed)) {
-            moveTriangle(0, speed);  
+            moveTriangle(0, speed);
         } else if (keyCode == LEFT && !willTriangleCrossLine(-speed, 0)) {
-            moveTriangle(-speed, 0);  
+            moveTriangle(-speed, 0);
         } else if (keyCode == RIGHT && !willTriangleCrossLine(speed, 0)) {
-            moveTriangle(speed, 0);  
+            
+                moveTriangle(speed, 0);
+            }
         }
-    }
+    
 
     public void moveTriangle(float deltaX, float deltaY) {
         x1 += deltaX;
@@ -67,19 +68,25 @@ public class App extends PApplet {
     }
 
     public boolean willTriangleCrossLine(float deltaX, float deltaY) {
-        
-        float newY1 = y1 + deltaY;
-        float newY2 = y2 + deltaY;
-        float newY3 = y3 + deltaY;
+
+        float newX1 = x1 + deltaX;
+        float newX2 = x2 + deltaX;
+        float newX3 = x3 + deltaX; 
 
         
-        if (newY1 < lineY1 || newY2 < lineY1 || newY3 < lineY1) {
-            return true;  
-
         
+        
+
+        System.out.println(newX1 > lineX1);
+        System.out.println(newX2 > lineX1);
+        System.out.println(newX3 > lineX1);
+
+        if (newX1 > lineX1 || newX2 > lineX1 || newX3 > lineX1) {
+            return true;
+
         }
-          return false;
-  }
+        return false; 
+
+        
+    }
 }
-
-
